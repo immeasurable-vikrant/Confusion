8373ca717c4abf90e144de64c0dcab8d57a756d0
vikrant <vikrant.immeasurable@gmail.com>
2019-02-07 01:04:31 +0530

    component part1 first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
       </div>
     );
   }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -49,12 +49,22 @@ class Menu extends Component {
 
     render() {
 
-        const menu = this.state.dishes.map(dish) => {
+        const menu = this.state.dishes.map ((dish) => {
 
-            return(
-                <div key = {dish.id} className = "col-12 mt-5"
+            return (
+                <div key = {dish.id} className = "col-12 mt-5">
+                <Media tag="li">
+                   <Media left middle>
+                     <Media object src={dish.image} alt={dish.name} />
+                      </Media>
+                    <Media body className="ml-5">
+                              <Media heading>{dish.name}</Media>
+                              <p>{dish.description}</p>
+                      </Media>
+                </Media>
+                </div>
             );
-        };
+        });
 
         return(
             <div classname="container">
