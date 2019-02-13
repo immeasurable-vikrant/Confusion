89723900661d5ce516c9fbc4dacd5135a5eedb35
vikrant <vikrant.immeasurable@gmail.com>
2019-02-14 00:26:37 +0530

    final component commit

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import { Card, CardImg, CardImgOverlay,CardBody, CardTitle, CardText} from 'reactstrap';
-=======
-import { Media } from 'reactstrap';
->>>>>>> origin/master
 
 class Menu extends Component {
 
@@ -11,10 +7,7 @@ class Menu extends Component {
         super(props);
 
         this.state = {
-<<<<<<< HEAD
-            
-            selectedDish:null
-            
+            selectedDish: null
         }
     }
 
@@ -22,68 +15,25 @@ class Menu extends Component {
         this.setState({ selectedDish: dish});
     }
 
-    renderDish(dish){
-        if(dish!=null) {
+    renderDish(dish) {
+        if (dish != null)
             return(
                 <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImg top src={dish.image} alt={dish.name} />
                     <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
                     </CardBody>
                 </Card>
             );
-
-        }
-        else {
+        else
             return(
                 <div></div>
             );
-=======
-            dishes: [
-                {id: 0,
-                    name:'Uthappizza',
-                    image: 'assets/images/uthappizza.png',
-                    category: 'mains',
-                    label:'Hot',
-                    price:'4.99',
-                    description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        
-                },
-                
-               {
-                  id: 1,
-                  name:'Zucchipakoda',
-                  image: 'assets/images/zucchipakoda.png',
-                  category: 'appetizer',
-                  label:'',
-                  price:'1.99',
-                  description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
-               {
-                  id: 2,
-                  name:'Vadonut',
-                  image: 'assets/images/vadonut.png',
-                  category: 'appetizer',
-                  label:'New',
-                  price:'1.99',
-                  description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
-               {
-                  id: 3,
-                  name:'ElaiCheese Cake',
-                  image: 'assets/images/elaicheesecake.png',
-                  category: 'dessert',
-                  label:'',
-                  price:'2.99',
-                  description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'    
-               }
-
-            ],
->>>>>>> origin/master
-        }
     }
 
     render() {
 
-<<<<<<< HEAD
         const menu = this.props.dishes.map ((dish) => {
 
             return (
@@ -118,36 +68,6 @@ class Menu extends Component {
 
 
          
-=======
-        const menu = this.state.dishes.map ((dish) => {
-
-            return (
-                <div key = {dish.id} className = "col-12 mt-5">
-                <Media tag="li">
-                   <Media left middle>
-                     <Media object src={dish.image} alt={dish.name} />
-                      </Media>
-                    <Media body className="ml-5">
-                              <Media heading>{dish.name}</Media>
-                              <p>{dish.description}</p>
-                      </Media>
-                </Media>
-                </div>
-            );
-        });
-
-        return(
-            <div classname="container">
-            <div classname="row">
-            <Media list>
-                {menu}
-            </Media>
-               </div>
-            </div>
-
-
-        );
->>>>>>> origin/master
     }
 }
 export default Menu;
