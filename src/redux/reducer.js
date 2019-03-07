6efc48e2da1307092b1e5ef52959391387db5037
vikrant <vikrant.immeasurable@gmail.com>
2019-03-07 21:48:05 +0530

    Redux Actions

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,3 @@
-import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';	
 																																																																																																																																																																																																																																																																																																																								import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
