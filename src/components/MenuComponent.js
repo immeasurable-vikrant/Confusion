10b3305c43dc041ac58388d7662a8da43fc59476
vikrant <vikrant.immeasurable@gmail.com>
2019-03-25 01:21:57 +0530

    final thunk commit

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb,BreadcrumbItem } f
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 
-function RenderMenuItem({ dish,onClick}) {
+function RenderMenuItem({ dish, onClick}) {
         
     return( 
         <Card >
@@ -28,23 +28,43 @@ function RenderMenuItem({ dish,onClick}) {
                 </div>
             );
         });
-        return (
-            <div className='container'>
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>Menu</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                    <h3> Menu</h3>
-                    <hr/>
+        
+        if(props.dishes.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <Loading />
                     </div>
                 </div>
-                <div className='row'>
-                    {menu}
+            );
+        }
+        else if (props.dishes.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.dishes.errMess}</h4>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        }
+        else
+         return (
+                <div className='container'>
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>Menu</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                        <h3> Menu</h3>
+                        <hr/>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        {menu}
+                    </div>
+                </div>
+            );
 
           
     }
