a5d527047ca18a4f89bb76a943c2ea83220092e4
vikrant <vikrant.immeasurable@gmail.com>
2019-03-24 18:09:22 +0530

    Loading Component Added

--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -10,11 +10,9 @@ export const Dishes = (state= {
         case ActionTypes.ADD_DISHES:
         return {...state, isLoading: false, errMess: null, dishes: action.payload}
 
-
         case ActionTypes.DISHES_LOADING:
         return {...state, isLoading: true, errMess: null, dishes: []}
-
-
+    
         case ActionTypes.DISHES_FAILED:
         return {...state, isLoading: false, errMess: action.payload, dishes: []}
 
