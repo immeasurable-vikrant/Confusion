7f75bc37c46419a4e133b24134bfb083c3e78d59
vikrant <vikrant.immeasurable@gmail.com>
2019-03-26 01:03:16 +0530

    React Redux Form

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { createStore,combineReducers, applyMiddleware  } from 'redux';
+import { createForms } from 'react-redux-form'
 import { Dishes } from './dishes';
 import { Promotions } from './promotions';
 import { Leaders } from './leaders';
 import { Comments } from './comments';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { InitialFeedback } from './forms';
 
 
 export const ConfigureStore = () => {
@@ -13,7 +15,10 @@ export const ConfigureStore = () => {
             dishes: Dishes,
             promotions: Promotions,
             comments: Comments,
-            leaders: Leaders
+            leaders: Leaders,
+            ...createForms({
+                feedback: InitialFeedback
+            })
         }),
         applyMiddleware(thunk, logger)
     );
